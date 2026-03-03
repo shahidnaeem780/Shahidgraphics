4ef1cd86441b79c3dc4ae2c88697a358e1e2426a
shahidnaeem780 <shahidnaeem780779@gmail.com>
2026-03-03 12:02:31 +0500

    Update youtube downloader.py

--- youtube downloader.py
+++ youtube downloader.py
@@ -1,8 +1,9 @@
 import yt_dlp
-
+# Paste your youtube video link
 url = "https://youtu.be/ZvYluToZvnA?si=8LYS7kv49C4tJh5X"
 
 ydl_opts = {}
 
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     ydl.download([url])
+
